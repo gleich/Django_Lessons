83cf8001df1ef43886d1f50c81ef2a5d4098bae3
Matt-Gleich <matthewgleich@gmail.com>
2019-04-22 21:15:48 -0400

    Update urls.py

--- Django_Lessons/urls.py
+++ Django_Lessons/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include(accounts.url))
+    path('account/', include("accounts.urls"))
 ]
